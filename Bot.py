24d7cc5fe140980b447dd8e7c39026ab42683d3c
D1gout <fanolkon@gmail.com>
2022-11-28 16:15:27 +0500

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,47 @@
+import logging
+import os
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
+
+
+TOKEN = os.getenv('5975391066:AAEHxpuSeVYz4fidfGbV61zuKN4zOrxGvDY')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('gu-time-bot')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
---
+++ Gu_Parcer.py
@@ -0,0 +1,21 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+# Токен, выданный BotFather в телеграмме
+API_TOKEN = 'BOT TOKEN HERE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
