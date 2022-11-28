463274de77a5b7b2215eed6158b6a1ed9821a336
D1gout <fanolkon@gmail.com>
2022-11-28 16:21:52 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,33 +1,16 @@
 import logging
-import os
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
-from aiogram import Bot, types
 
+from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = os.getenv('5975391066:AAEHxpuSeVYz4fidfGbV61zuKN4zOrxGvDY')
-bot = Bot(token=TOKEN)
+# Токен, выданный BotFather в телеграмме
+API_TOKEN = '5975391066:AAEHxpuSeVYz4fidfGbV61zuKN4zOrxGvDY'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-HEROKU_APP_NAME = os.getenv('gu-time-bot')
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
-
 
 @dp.message_handler()
 async def echo(message: types.Message):
@@ -35,13 +18,4 @@ async def echo(message: types.Message):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp, skip_updates=True)
