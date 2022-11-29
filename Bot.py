bc2feef4ae9c4402726d152cc5097f4968e7f1d7
D1gout <fanolkon@gmail.com>
2022-11-29 12:19:47 +0500

    update

--- Bot.py
+++ Bot.py
@@ -1,27 +1,17 @@
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-
-class MediaIds(Base):
-    __tablename__ = 'Media ids'
-    id = Column(Integer, primary_key=True)
-    file_id = Column(Sastring(255))
-    filename = Column(String(255))
-
 
 # Токен, выданный BotFather в телеграмме
-API_TOKEN = '5975391066:AAEHxpuSeVYz4fidfGbV61zuKN4zOrxGvDY'
+TOKEN = '5975391066:AAEHxpuSeVYz4fidfGbV61zuKN4zOrxGvDY'
+MY_ID = '706967790'
+DB_FILENAME = 'db_bot'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -33,14 +23,6 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
-
-
-@dp.message_handler(commands=['photo'])
-async def process_photo_command(message: types.Message):
-    caption = 'Какие глазки! :eyes:'
-    await bot.send_photo(message.from_user.id, CAT_BIG_EYES,
-                         caption=emojize(caption),
-                         reply_to_message_id=message.message_id)
 
 
 @dp.message_handler()
